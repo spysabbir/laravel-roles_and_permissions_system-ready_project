dddd3519959a0a14821fcce6fc5efde3d2f73021
spysabbir <sabbirahammedsovon@gmail.com>
2023-07-27 00:17:04 +0600

    Setup

--- routes/admin.php
+++ routes/admin.php
@@ -6,33 +6,37 @@ use App\Http\Controllers\Auth\NewPasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('guest')->group(function () {
-    Route::get('login', [AuthenticatedSessionController::class, 'create'])->name('login');
-    Route::post('login', [AuthenticatedSessionController::class, 'store']);
+Route::prefix('admin')->name('admin.')->group(function(){
 
-    Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])->name('password.request');
-    Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])->name('password.email');
-    Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])->name('password.reset');
-    Route::post('reset-password', [NewPasswordController::class, 'store'])->name('password.store');
-});
+    Route::middleware('guest')->group(function () {
+        Route::get('login', [AuthenticatedSessionController::class, 'create'])->name('login');
+        Route::post('login', [AuthenticatedSessionController::class, 'store']);
 
-Route::middleware('auth')->group(function () {
-    Route::get('confirm-password', [ConfirmablePasswordController::class, 'show'])->name('password.confirm');
-    Route::post('confirm-password', [ConfirmablePasswordController::class, 'store']);
+        Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])->name('password.request');
+        Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])->name('password.email');
+        Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])->name('password.reset');
+        Route::post('reset-password', [NewPasswordController::class, 'store'])->name('password.store');
+    });
 
-    Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
+    Route::middleware('auth')->group(function () {
+        Route::get('confirm-password', [ConfirmablePasswordController::class, 'show'])->name('password.confirm');
+        Route::post('confirm-password', [ConfirmablePasswordController::class, 'store']);
 
-    Route::resource('permission', PermissionController::class);
+        Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
 
-    Route::resource('role', RoleController::class);
+        Route::resource('permission', PermissionController::class);
 
-    Route::get('/assign/role/permission', [AdminController::class, 'AssignRolePermission'])->name('assign.role.permission');
-    Route::post('/assign/role/permission', [AdminController::class, 'AssignRolePermissionStore'])->name('assign.role.permission.store');
-    Route::get('/edit/role/permission/{id}', [AdminController::class, 'EditRolePermission'])->name('edit.role.permission');
-    Route::post('/edit/role/permission/{id}', [AdminController::class, 'UpdateRolePermission'])->name('edit.role.permission.update');
+        Route::resource('role', RoleController::class);
 
-    Route::get('/all/admin', [AdminController::class, 'AllAdmin'])->name('all.admin');
-    Route::post('/admin/store', [AdminController::class, 'AdminStore'])->name('admin.store');
-    Route::get('/admin/edit/{id}', [AdminController::class, 'AdminEdit'])->name('admin.edit');
-    Route::post('/admin/update/{id}', [AdminController::class, 'AdminUpdate'])->name('admin.update');
+        Route::get('/assign/role/permission', [AdminController::class, 'AssignRolePermission'])->name('assign.role.permission');
+        Route::post('/assign/role/permission', [AdminController::class, 'AssignRolePermissionStore'])->name('assign.role.permission.store');
+        Route::get('/edit/role/permission/{id}', [AdminController::class, 'EditRolePermission'])->name('edit.role.permission');
+        Route::post('/edit/role/permission/{id}', [AdminController::class, 'UpdateRolePermission'])->name('edit.role.permission.update');
+
+        Route::get('/all/admin', [AdminController::class, 'AllAdmin'])->name('all.admin');
+        Route::post('/admin/store', [AdminController::class, 'AdminStore'])->name('admin.store');
+        Route::get('/admin/edit/{id}', [AdminController::class, 'AdminEdit'])->name('admin.edit');
+        Route::post('/admin/update/{id}', [AdminController::class, 'AdminUpdate'])->name('admin.update');
+    });
+
 });
